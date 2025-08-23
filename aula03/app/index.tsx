536f7a81c40b743b98fbe909a9aab074af1ba37e
Vinícius Brandão <vnbrandaoambrosio@gmail.com>
2025-08-22 22:20:26 -0300

    subindo aula03 expo

---
+++ aula03/app/index.tsx
@@ -0,0 +1,9 @@
+import { Button, ButtonText } from "@/components/ui/button";
+
+export default function Index() {
+  return (
+    <Button size="lg" variant="solid" action="positive">
+      <ButtonText>CONECTAR</ButtonText>
+    </Button>
+  );
+}
